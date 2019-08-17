798135cf3370986cfacad61d43b16388f548f361
avico83 <avi.cohen83@gmail.com>
2019-08-17 20:06:06 +0300

    Update static_route_tracking.py

--- static_route_tracking.py
+++ static_route_tracking.py
@@ -7,7 +7,7 @@ import syslog
 switch = Server("unix:/var/run/command-api.sock")
 
 def ping_service(dst, next_hop, name):
-    ping = switch.runCmds(1, ["ping {}".format(next_hop).replace(',', ' ')])
+    ping = switch.runCmds(1, ["ping {} -i 1 -c 4".format(next_hop).replace(',', ' ')])
     ping = ping[0]['messages'][0]
     if '100% packet loss' in ping:
         syslog.openlog('Next-Hop {} is unreachable, we remove the routes of {}\n'.format(next_hop, dst), 0, syslog.LOG_LOCAL4)
